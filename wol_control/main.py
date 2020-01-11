8a7a8df39582a927fc5bef110eef213fefe790de
TheLastBilly <jtmonegro@gmail.com>
2020-01-11 16:32:16 -0500

    removed wol fixes

--- wol_control/main.py
+++ wol_control/main.py
@@ -5,8 +5,6 @@ from .models import Mac
 
 main = Blueprint('main', __name__)
 
-local_ip = "192.168.1.1"
-
 def get_controls( ):
     controls = []
     if current_user.admin:
@@ -31,7 +29,7 @@ def toggle_control(public_id, command):
         return render_template('control.html', message="No such controller found")
     
     if command == "on":
-        os.system("/usr/local/bin/wol -r {} {}".format(local_ip, control.mac))
+        os.system("/usr/local/bin/wol -r {}".format(control.mac))
         return render_template('control.html', message="Turning {} on".format(control.name), controls=[control], redirect_home = True)
     else:
         return render_template('control.html', message="Invalid command", controls=[control], redirect_home = True)
